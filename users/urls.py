1b8ce9d7f4e8928e3e657d4840b33ae39221ec69
mrnoob0dy <114588321+mrnoob0dy@users.noreply.github.com>
2023-04-21 18:41:23 +0300

    Add files via upload

---
+++ users/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from users.views import UserRegistrationView, UserProfileView, UserLoginView, EmailVerificationView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LogoutView
+
+
+app_name = 'users'
+
+urlpatterns = [
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('profile/<int:pk>/', login_required(UserProfileView.as_view()), name='profile'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('verify/<str:email>/<uuid:code>/', EmailVerificationView.as_view(), name='email_verification'),
+]
